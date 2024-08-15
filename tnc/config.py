ae51334704ba90d1a578ed4216b1a586ac40987e
Austin Orr <aorr@geosyntec.com>
2024-08-15 14:27:27 -0700

    load new pet data and rename data dir

--- tnc/config.py
+++ tnc/config.py
@@ -4,9 +4,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    PERLND: Path = Path(__file__).resolve().parent / "wwhm_data" / "WWHM_PERLNDs.csv"
-    IMPLND: Path = Path(__file__).resolve().parent / "wwhm_data" / "WWHM_IMPLNDs.csv"
-    TEMP_EVAP: Path = Path(__file__).resolve().parent / "wwhm_data" / "_temp_evap.csv"
+    PERLND: Path = Path(__file__).resolve().parent / "data" / "WWHM_PERLNDs.csv"
+    IMPLND: Path = Path(__file__).resolve().parent / "data" / "WWHM_IMPLNDs.csv"
+    TEMP_EVAP: Path = Path(__file__).resolve().parent / "data" / "_temp_evap.csv"
+    PET_MM_DAILY: Path = Path(__file__).resolve().parent / "data" / "pet_mm_daily.csv"
     GOOGLE_APPLICATION_CREDENTIALS_JSON: str = ""
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
